a8f33f13c97ae09d36e4787d9052e57048b90061
Steven Inouye <hiiamsteveni@gmail.com>
2022-07-09 16:09:23 -0700

    Force SWC Transfomation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    // https://nextjs.org/docs/messages/swc-disabled
+    forceSwcTransforms: true,
+  },
+};
 
 module.exports = nextConfig
